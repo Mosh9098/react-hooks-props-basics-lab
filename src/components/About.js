b0f4db85624366d726c1a44ef914d72bd74268d1
mosh9098 <mosh9098@gmail.com>
2024-04-25 23:44:27 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About({bio,github,linkedin}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{bio}</p>
+      {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={github} linkedin={linkedin} />
 
